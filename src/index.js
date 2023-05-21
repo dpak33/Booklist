548b72be77858696fdac9f4d723373642c96879e
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-21 17:23:43 +0100

    2nd

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const BookList = () => {
-  return <section></section>
+  return (<section className="booklist">
+    <Book />
+  </section>)
 }
 
 const Book = () => {
-  return <article>
-
+  return <article className="book">
+    <Image />
+    <Title />
+    <Author />
 
   </article>
 }
 
-const Image = () => <h2>image placeholder</h2>
+const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/81iw2hmh35L._AC_UL900_SR900,600_.jpg" />
+const Title = () => <h2>Atlas</h2>
+const Author = () => <h2>Lucinda Riley</h2>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-
+  <BookList />
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
