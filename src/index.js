1721695ba4736b122c8c80fffb2af87b698a23fa
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-22 14:24:35 +0100

    more

--- src/index.js
+++ src/index.js
@@ -2,21 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const firstBook = {
+  author: 'Lucinda Reilly',
+  title: 'Atlas: the story',
+  img: 'https://m.media-amazon.com/images/I/51dM3mS1wdL._SX328_BO1,204,203,200_.jpg'
+};
+
+const secondBook = {
+  author: 'Jessie Inchauspé ',
+  title: 'The Glucose Goddess Method',
+  img: 'https://m.media-amazon.com/images/I/5105MIgJQuL._SX382_BO1,204,203,200_.jpg'
+};
+
 const BookList = () => {
   return (<section className="booklist">
-    <Book />
+    <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}>
+      <p>Hello text</p>
+      <button>Click me!</button>
+    </Book>
+
+    <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
   </section>)
 }
 
-const Book = () => {
-  const title = 'Atlas: the story';
-  const author = "Lucinda Reilly";
+const Book = (props) => {
+  const { img, title, author, children } = props;
+
   return (
     <article className="book">
-      <img src='./images/book1.jpg' />
+      <img
+        src={img}
+        alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <p>{2 - 1}</p>
+      {children}
     </article>
   );
 };
