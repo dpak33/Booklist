f27a1eba9886e5023ad06da21db82810dfded091
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-23 13:04:57 +0100

    Another commit

--- src/index.js
+++ src/index.js
@@ -2,31 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const firstBook = {
-  author: 'Lucinda Reilly',
-  title: 'Atlas: the story',
-  img: 'https://m.media-amazon.com/images/I/51dM3mS1wdL._SX328_BO1,204,203,200_.jpg'
-};
+const books = [
+  {
+    author: 'Lucinda Reilly',
+    title: 'Atlas: the story',
+    img: 'https://m.media-amazon.com/images/I/51dM3mS1wdL._SX328_BO1,204,203,200_.jpg',
+    id: 1,
+  },
+  {
+    author: 'Jessie Inchauspé ',
+    title: 'The Glucose Goddess Method',
+    img: 'https://m.media-amazon.com/images/I/5105MIgJQuL._SX382_BO1,204,203,200_.jpg',
+    id: 2,
+  },
+];
 
-const secondBook = {
-  author: 'Jessie Inchauspé ',
-  title: 'The Glucose Goddess Method',
-  img: 'https://m.media-amazon.com/images/I/5105MIgJQuL._SX382_BO1,204,203,200_.jpg'
-};
 
 const BookList = () => {
   return (<section className="booklist">
-    <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}>
-      <p>Hello text</p>
-      <button>Click me!</button>
-    </Book>
-
-    <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+    <EventExamples />
+    {books.map((book) => {
+      return (
+        <Book {...book} key={book.id} />
+      )
+    })}
   </section>)
-}
+};
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input')
+  };
+
+  const handleButtonClick = () => {
+    alert('handle button click')
+  };
+
+  return <section>
+    <form>
+      <h2>Typical form</h2>
+      <input type="text" name='example' onChange={handleFormInput} style={{ margin: '1rem 0' }} />
+    </form>
+    <button onClick={handleButtonClick}>click me!</button>
+  </section>
+};
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
 
   return (
     <article className="book">
@@ -35,7 +57,6 @@ const Book = (props) => {
         alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
