2503f4567f171826833daf93b6fefd6dadd79576
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-24 10:57:18 +0100

    y

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+    {
+        author: 'Lucinda Reilly',
+        title: 'Atlas: the story',
+        img: 'https://m.media-amazon.com/images/I/51dM3mS1wdL._SX328_BO1,204,203,200_.jpg',
+        id: 1,
+    },
+    {
+        author: 'Jessie Inchauspé ',
+        title: 'The Glucose Goddess Method',
+        img: 'https://m.media-amazon.com/images/I/5105MIgJQuL._SX382_BO1,204,203,200_.jpg',
+        id: 2,
+    },
+];
--- src/index.js
+++ src/index.js
@@ -1,57 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-
-const books = [
-  {
-    author: 'Lucinda Reilly',
-    title: 'Atlas: the story',
-    img: 'https://m.media-amazon.com/images/I/51dM3mS1wdL._SX328_BO1,204,203,200_.jpg',
-    id: 1,
-  },
-  {
-    author: 'Jessie Inchauspé ',
-    title: 'The Glucose Goddess Method',
-    img: 'https://m.media-amazon.com/images/I/5105MIgJQuL._SX382_BO1,204,203,200_.jpg',
-    id: 2,
-  },
-];
-
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
-
-  const getBook = (id) => {
-    const book = books.find(el => el.id === id);
-    console.log(book.title);
-  };
 
   return (<section className="booklist">
     {books.map((book) => {
       return (
-        <Book {...book} key={book.id} getBook={() => getBook(book.id)} />
+        <Book {...book} key={book.id} />
       )
     })}
 
   </section>)
 };
-
-
-const Book = (props) => {
-  const { img, title, author, getBook } = props;
-
-  return (
-    <article className="book">
-      <img
-        src={img}
-        alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getBook}>click me!</button>
-      <h4>{author}</h4>
-    </article>
-  );
-};
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
