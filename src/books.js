03725dd73546c33d97e8a068e7b0c04b5e937574
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-24 11:11:03 +0100

    y

--- src/books.js
+++ src/books.js
@@ -1,14 +1,25 @@
+import img1 from './booklist images/book1.jpg';
+import img2 from './booklist images/book2.jpg';
+import img3 from './booklist images/book3.jpg';
+
+
 export const books = [
     {
         author: 'Lucinda Reilly',
         title: 'Atlas: the story',
-        img: 'https://m.media-amazon.com/images/I/51dM3mS1wdL._SX328_BO1,204,203,200_.jpg',
+        img: img1,
         id: 1,
     },
     {
         author: 'Jessie Inchauspé ',
         title: 'The Glucose Goddess Method',
-        img: 'https://m.media-amazon.com/images/I/5105MIgJQuL._SX382_BO1,204,203,200_.jpg',
+        img: img2,
         id: 2,
+    },
+    {
+        author: 'Davina Taylor',
+        title: 'Hack Your Hormones',
+        img: img3,
+        id: 3,
     },
 ];
